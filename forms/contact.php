10d3fb580a4b3f2dba273ba27600fa5143770972
Lucas David Erkana <lderkana21x@gmail.com>
2024-06-19 15:04:37 +0200

    Contact form update

--- forms/contact.php
+++ forms/contact.php
@@ -3,11 +3,11 @@
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
   * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://https://aimarketing.com.na//php-email-form/
+  * For more info and help: https://aimarketing.com.na/php-email-form/
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'erkanalucas@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
